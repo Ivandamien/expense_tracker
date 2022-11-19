4ee986ed75da30f29975756a3904c17cc8bccf43
Ivan Okello <ivandamieno99@gmail.com>
2022-11-19 17:07:00 +0300

    Outputting conditional content with filtering expenses by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,27 +1,42 @@
-import {React,useState} from 'react'
+import React, { useState } from 'react';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 
 const Expenses = (props) => {
+
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  // let expensesContent = <p>No expenses found</p>
+  // if
+
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {props.items.map((expense) =>(
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+
+      { filteredExpenses.length === 0 && <p> No expenses found. </p> }
+      {filteredExpenses.length > 0 && (
+        filteredExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
         />
-      ))}
+      ))) };
+      {}
       
            {/* <ExpenseItem
               title={props.items[0].title} 
