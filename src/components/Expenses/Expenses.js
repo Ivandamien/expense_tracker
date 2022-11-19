b14ba75e6b9d5eb8e32bf4b266ade8c1dce846e8
Ivan Okello <ivandamieno99@gmail.com>
2022-11-19 19:00:29 +0300

    Did some refactors on expenses and expensesList component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css'
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 
 const Expenses = (props) => {
@@ -26,29 +26,10 @@ const Expenses = (props) => {
         onChangeFilter={filterChangeHandler}
       />
 
-      { filteredExpenses.length === 0 && <p> No expenses found. </p> }
-      {filteredExpenses.length > 0 && (
-        filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))) };
-      {}
+      <ExpensesList items={ filteredExpenses} />
       
-           {/* <ExpenseItem
-              title={props.items[0].title} 
-              amount={props.items[0].amount} 
-              date={props.items[0].date}
-              />
-           <ExpenseItem
-              title={props.items[1].title} 
-              amount={props.items[1].amount} 
-              date={props.items[1].date}
-              /> */}
-      </Card>
+      
+    </Card>
   )
 
 }
--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import './ExpenseList.css'
 
-const ExpensesList = () => {
-  return (
-    <div>ExpensesList</div>
-  )
+import ExpenseItem from './ExpenseItem'
+
+const ExpensesList = (props) => {
+
+    // if(props.items.length === 0){
+    //     return <h2 className='expenses-list__fallback'>Found no expenses</h2>
+    // };
+  
+    
+    
+    return <ul className="expenses-list">
+    
+            { props.items.map((expense) => (
+                <ExpenseItem
+                    key={expense.id}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />
+            ))};
+           </ul>
+  
 }
 
 export default ExpensesList
