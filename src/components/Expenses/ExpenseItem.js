fb11dd9d1fb2b1c7e3d2660e8a1ddc9bdf7f3771
Ivan Okello <ivandamieno99@gmail.com>
2022-11-20 01:36:59 +0300

    Minor bug fixes

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,17 +8,17 @@ const ExpenseItem = (props) => {
 
     
     return (
-        <ul>
-
         
-        <Card className="expense-item">
+<li>
+            <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{ props.title}</h2>
                 <div className="expense-item__price">{ props.amount}</div>
             </div>
             </Card>
-        </ul>
+            </li>
+        
     );
 };
 
